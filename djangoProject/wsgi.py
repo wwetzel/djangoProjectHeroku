4780f06fdf8f5ab26d0ebd08b6e73dbbb64255b5
WillieWetz <wcwetzel@unomaha.edu>
2018-01-20 18:58:59 -0600

    yoo

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings")
